39ef6fbb2b2e6165b03534242e3ffc77dd4998fd
[Raul Ocaña Taboada] <rocanata@uoc.edu>
2025-11-28 19:26:55 +0100

    Netlify

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:5173')], //[env('FRONTEND_URL', 'http://localhost:3000')],
+    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:5173', 'https://oast-web.netlify.app')], //[env('FRONTEND_URL', 'http://localhost:3000')],
 
     'allowed_origins_patterns' => [],
 
